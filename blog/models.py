1078d50dabecf0cecd31af7295ae0123e6cca17e
YoonJ <oyoons95@gmail.com>
2018-11-04 23:47:32 +0900

    commit-

---
+++ blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+
+def user_path(instance, filename):
+    from random import choice
+    import string
+
+    arr = [choice(string.ascii_letters) for _ in range(8)]
+    pid = ''.join(arr)
+    extension = filename.split('.')[-1]
+    return '%s/%s.%s'%(instance.author.username, pid, extension)
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to= user_path)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return f'Post (PK: {self.pk}, Author: {self.author.username})'
---
+++ blog/views.py
@@ -0,0 +1,111 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as django_login, logout as django_logout, authenticate
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Post
+from .forms import PostForm
+
+from .forms import UserForm, LoginForm,InviteForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponse
+from django.core.mail import EmailMessage
+
+@login_required
+def invite(request):
+    if request.method == "POST":
+        form = InviteForm(request.POST)
+        username = request.POST['username']
+        email = request.POST['email']
+        if form.is_valid():
+            subject = '[초대장] No Name to'+username
+            message = '초대장 입니다.' \
+                      'http://127.0.0.1:8000/accounts/signup/'
+
+            mail = EmailMessage(subject, message, to=[email])
+            mail.send()
+            return redirect('post_list')
+    else:
+        return render(request, 'registration/invite.html', {'form': InviteForm})
+
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create_user(**form.cleaned_data)
+            login(request, new_user)
+            return redirect('post_list')
+    else:
+        form = UserForm()
+        return render(request, 'registration/signup.html', {'form': form})
+
+def signin(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('post_list')
+        else:
+            return HttpResponse('로그인 실패. 다시 시도 해보세요.')
+    else:
+        form = LoginForm()
+        return render(request, 'registration/login.html', {'form': form})
+
+@login_required
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+@login_required
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form':form})
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts':posts})
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
