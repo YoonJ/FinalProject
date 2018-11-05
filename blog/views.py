e99c833f39c88e857da7e13af17559be8b259e94
YoonJ <oyoons95@gmail.com>
2018-11-05 15:59:13 +0900

    third commit

--- blog/views.py
+++ blog/views.py
@@ -58,7 +58,7 @@ def signin(request):
 
 @login_required
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 @login_required
